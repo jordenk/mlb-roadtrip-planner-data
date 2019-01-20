a735a707ab98417838035ffa10e62e901fce4daa
Jorden Kawamoto <jordenkawamoto@gmail.com>
2019-01-20 15:37:45 -0700

    remove data_types. dont need a separate package for now.

--- mlb_trip_planner_data/data_types/game.py
+++
@@ -1,8 +0,0 @@
-class Game:
-    def __init__(self, home_team, away_team, stadium_lat_long, timestamp):
-        self.home_team = home_team
-        self.away_team = away_team
-        self.stadium_lat_long = stadium_lat_long_map[home_team]
-        self.timestamp = timestamp
-
-stadium_lat_long_map = {}
--- mlb_trip_planner_data/data_types/team.py
+++
@@ -1,4 +0,0 @@
-class Team:
-  def __init__(self, urlName):
-    self.urlName = urlName
-    self.url = f"https://www.mlb.com/{urlName}/fans/downloadable-schedule"
